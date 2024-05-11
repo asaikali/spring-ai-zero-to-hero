869fad5fe866d9479c77ecadcd1dc7cb3997ff87
Adib Saikali <asaikali@gmail.com>
2024-05-11 14:07:27 -0400

    Polish

--- chat/ollama/src/main/java/com/example/OllamaChatApplication.java
+++ chat/ollama/src/main/java/com/example/OllamaChatApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OllamaTellMeAJokeApplication  {
+public class OllamaChatApplication {
     public static void main(String[] args) {
-        SpringApplication.run(OllamaTellMeAJokeApplication.class, args);
+        SpringApplication.run(OllamaChatApplication.class, args);
     }
 }
