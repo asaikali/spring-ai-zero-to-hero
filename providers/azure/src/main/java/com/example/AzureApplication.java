14e99a40ae4b32e8d05555c2fbb3a92d6e30a759
Adib Saikali <asaikali@gmail.com>
2024-05-11 23:27:25 -0400

    polish

--- providers/azure/src/main/java/com/example/AzureApplication.java
+++ providers/azure/src/main/java/com/example/AzureApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AzureOpenAiApplication {
+public class AzureApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AzureOpenAiApplication.class, args);
+        SpringApplication.run(AzureApplication.class, args);
     }
 }
