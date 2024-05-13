c9483cc45c9f3da07f764c842b257588638f5d95
Adib Saikali <asaikali@gmail.com>
2024-05-13 14:17:59 -0400

    polish

--- embedding/src/main/java/com/example/embed_01/BasicEmbeddingController.java
+++ embedding/src/main/java/com/example/embed_01/BasicEmbeddingController.java
@@ -1,20 +1,24 @@
 package com.example.embed_01;
 
 import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.embedding.EmbeddingClient;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/embed/01")
 public class BasicEmbeddingController {
 
-    private final ChatClient chatClient;
+    private final EmbeddingClient embeddingClient;
 
-    public BasicEmbeddingController(ChatClient chatClient) {
-        this.chatClient = chatClient;
+    public BasicEmbeddingController(EmbeddingClient embeddingClient) {
+        this.embeddingClient = embeddingClient;
     }
 
+
     @GetMapping("joke")
-    public String getJoke() {
-        return chatClient.call("Tell me a joke");
+    public List<Double> getJoke() {
+        return embeddingClient.embed("Tell me a joke");
     }
 }
