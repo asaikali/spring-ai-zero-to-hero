4a9c1067bd49dea70eb68f32d874904039d5ffb3
Adib Saikali <asaikali@gmail.com>
2024-05-13 13:47:26 -0400

    Polish

---
+++ chat/src/main/java/com/example/chat_06/RoleController.java
@@ -0,0 +1,64 @@
+package com.example.chat_06;
+
+import com.example.chat._02.PromptController;
+import org.apache.catalina.User;
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.Generation;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/chat/06")
+public class RoleController {
+    private final ChatClient chatClient;
+    private final String personaPrompt = """
+        You are a helpful experts on plants, however you are not allowed
+        to answers any question about vegetables you can only
+        answer questions about fruits.
+        """;
+
+    public RoleController(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @GetMapping("/fruit")
+    public String fruitQuestion(){
+        SystemMessage systemMessage = new SystemMessage(personaPrompt);
+        UserMessage userMessage = new UserMessage("What is the color of a banana?");
+        Prompt prompt = new Prompt(List.of(systemMessage,userMessage));
+
+        ChatResponse response = chatClient.call(prompt);
+        Generation generation = response.getResult();
+        AssistantMessage assistantMessage = generation.getOutput();
+
+        return assistantMessage.getContent();
+    }
+
+    @GetMapping("/veg")
+    public String vegetableQuestion(){
+        SystemMessage systemMessage = new SystemMessage(personaPrompt);
+        UserMessage userMessage = new UserMessage("What is the color of a carrot?");
+        Prompt prompt = new Prompt(List.of(systemMessage,userMessage));
+
+        System.out.println(prompt.getContents());
+
+        ChatResponse response = chatClient.call(prompt);
+        Generation generation = response.getResult();
+        AssistantMessage assistantMessage = generation.getOutput();
+
+        prompt = new Prompt(List.of(systemMessage,userMessage,assistantMessage));
+        return assistantMessage.getContent();
+
+    }
+}
