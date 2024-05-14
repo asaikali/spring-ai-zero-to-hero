42cfcf2ba8ea506d5dadb33aa2f88bbe4a791f20
Adib Saikali <asaikali@gmail.com>
2024-05-14 10:28:32 -0400

    Polish

---
+++ audio/src/main/java/com/example/audio_01/TranscribeController.java
@@ -0,0 +1,43 @@
+package com.example.embed_01;
+
+import java.util.List;
+import java.util.Map;
+import org.springframework.ai.embedding.EmbeddingClient;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionClient;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
+import org.springframework.ai.openai.api.OpenAiAudioApi;
+import org.springframework.ai.openai.audio.transcription.AudioTranscriptionPrompt;
+import org.springframework.ai.openai.audio.transcription.AudioTranscriptionResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/audio/01")
+public class TranscribeController {
+
+  private final OpenAiAudioTranscriptionClient transcriptionClient;
+
+  @Value("classpath:/data/The_Astronomer_Vermeer.ogg")
+  private Resource audioResource;
+
+  public TranscribeController( OpenAiAudioTranscriptionClient transcriptionClient) {
+      this.transcriptionClient = transcriptionClient;
+  }
+
+  @GetMapping("text")
+  public String transcribe() {
+
+    var transcriptionOptions = OpenAiAudioTranscriptionOptions.builder()
+            .withResponseFormat(OpenAiAudioApi.TranscriptResponseFormat.TEXT)
+            .withTemperature(0f)
+            .build();
+
+    AudioTranscriptionPrompt transcriptionRequest = new AudioTranscriptionPrompt(audioResource, transcriptionOptions);
+    AudioTranscriptionResponse response = this.transcriptionClient.call(transcriptionRequest);
+
+    return response.getResult().getOutput();
+  }
+
+
+}
