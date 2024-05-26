a438702e5c3907fef40f91b601ef6f5a33511d6d
Adib Saikali <asaikali@gmail.com>
2024-05-26 13:25:58 +0200

    Polish

---
+++ pgvector/docker_postgres_init.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE demo
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+CREATE EXTENSION vector;
