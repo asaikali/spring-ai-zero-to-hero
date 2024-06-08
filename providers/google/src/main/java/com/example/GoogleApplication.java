2edb6934c89c6d1e54796b3ba602f585a8e1618e
Adib Saikali <asaikali@gmail.com>
2024-06-08 08:46:48 -0400

    add google

---
+++ providers/google/src/main/java/com/example/GoogleApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(GoogleApplication.class, args);
+  }
+}
