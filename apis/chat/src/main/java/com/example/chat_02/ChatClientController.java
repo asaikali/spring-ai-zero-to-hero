934c510b4f651b962d1fee57393e73274e85c8ed
Adib Saikali <asaikali@gmail.com>
2024-10-29 13:22:53 -0400

    Polish

--- apis/chat/src/main/java/com/example/chat_02/ChatClientController.java
+++ apis/chat/src/main/java/com/example/chat_02/ChatClientController.java
@@ -6,7 +6,6 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.model.Generation;
-import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
