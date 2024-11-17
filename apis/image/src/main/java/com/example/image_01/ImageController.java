034e9c87403c1699c0b5c920c18299f1f6cb29f4
Adib Saikali <asaikali@gmail.com>
2024-11-17 18:04:18 -0500

    Polish

--- apis/image/src/main/java/com/example/image_01/ImageController.java
+++ apis/image/src/main/java/com/example/image_01/ImageController.java
@@ -2,9 +2,10 @@ package com.example.image_01;
 
 import org.springframework.ai.image.Image;
 import org.springframework.ai.image.ImageModel;
+import org.springframework.ai.image.ImageOptions;
+import org.springframework.ai.image.ImageOptionsBuilder;
 import org.springframework.ai.image.ImagePrompt;
 import org.springframework.ai.image.ImageResponse;
-import org.springframework.ai.openai.OpenAiImageOptions;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,8 +27,7 @@ public class ImageController {
         imageModel.call(
             new ImagePrompt(
                 "A light cream colored mini golden doodle",
-                OpenAiImageOptions.builder()
-                    .withQuality("hd")
+                ImageOptionsBuilder.builder()
                     .withN(1)
                     .withHeight(1024)
                     .withWidth(1024)
