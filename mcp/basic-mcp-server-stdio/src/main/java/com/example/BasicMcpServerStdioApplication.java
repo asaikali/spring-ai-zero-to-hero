301b6f37f4efb84c35c9dc819ca3bfe476d1f9ea
Adib Saikali <asaikali@gmail.com>
2025-03-30 20:29:20 -0400

    add a basic mcp-stdio-server

---
+++ mcp/basic-mcp-server-stdio/src/main/java/com/example/BasicMcpServerStdioApplication.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BasicMcpServerStdioApplication {
+
+  @Bean
+  public ToolCallbackProvider weatherTools(WeatherService weatherService) {
+    return MethodToolCallbackProvider.builder().toolObjects(weatherService).build();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(BasicMcpServerStdioApplication.class, args);
+  }
+}
---
+++ mcp/basic-mcp-server-stdio/src/main/java/com/example/WeatherService.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WeatherService {
+
+  @Tool(description = "Get weather information by city name")
+  public String getWeather(String cityName) {
+    return "It is currently sunny and a pleasent 25 degrees in " + cityName;
+  }
+}
