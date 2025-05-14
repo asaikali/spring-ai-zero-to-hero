45563808f13672ad08304250724c8588a0b7f3d3
Adib Saikali <asaikali@gmail.com>
2025-05-14 00:05:58 -0400

    Polish

--- components/apis/chat/src/main/java/com/example/chat_05/TimeTools.java
+++ components/apis/chat/src/main/java/com/example/chat_05/TimeTools.java
@@ -9,10 +9,10 @@ import org.springframework.ai.tool.annotation.ToolParam;
 public class TimeTools {
 
   @Tool(
-      description = "Returns the current time in a specific time for an IANA time zone identifier")
+      description = "Returns the current time in a specific time for an IANA time zone identifier", returnDirect = false)
   public String currentTimeIn(
       @ToolParam(required = false, description = "IANA time zone identifiers") String timeZone) {
-    ZoneId zoneId = ZoneId.of("Europe/Paris");
+    ZoneId zoneId = ZoneId.of(timeZone);
     ZonedDateTime now = ZonedDateTime.now(zoneId);
     return now.format(DateTimeFormatter.ISO_LOCAL_TIME);
   }
