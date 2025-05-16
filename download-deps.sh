c2d54ef1d3f272e253e978d013eb9e348e34d251
Ztefan <ztefan.lundgren@gmail.com>
2025-05-17 00:28:24 +0200

    fix file not found issue

--- download-deps.sh
+++ download-deps.sh
@@ -1,5 +1,5 @@
 ollama pull llama4
 ollama pull mxbai-embed-large
-docker-compose  -f docker/postgres/docker-compose.yml pull
-docker-compose  -f docker/observability-stack/docker-compose.yml pull
+docker-compose  -f docker/postgres/docker-compose.yaml pull
+docker-compose  -f docker/observability-stack/docker-compose.yaml pull
 ./mvnw clean package
