94958ef248b5cd9dee04d0855f72b6881f666add
Adib Saikali <asaikali@gmail.com>
2025-05-21 01:01:32 +0200

    cleanup

--- components/patterns/04-agentic-loop/src/main/java/com/example/agentic/memgpt/MemoryManager.java
+++
@@ -1,9 +0,0 @@
-package com.example.agentic.memgpt;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class MemoryManager {
-
-  private Map<String, MemoryBlock> memory = new HashMap<>();
-}
