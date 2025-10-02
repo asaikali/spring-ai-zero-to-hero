8715d57a29e438e7be373c46fa13b19eda937ec5
Neven Cvetkovic <neven.cvetkovic@gmail.com>
2025-10-02 21:38:30 +0200

    Fixed typos with docker-compose yaml filenames

--- download-deps.sh
+++ download-deps.sh
@@ -1,6 +1,6 @@
 ollama pull llama3.2
 ollama pull llava
 ollama pull mxbai-embed-large
-docker-compose  -f docker/postgres/docker-compose.yml pull
-docker-compose  -f docker/observability-stack/docker-compose.yml pull
+docker-compose  -f docker/postgres/docker-compose.yaml pull
+docker-compose  -f docker/observability-stack/docker-compose.yaml pull
 ./mvnw clean package
